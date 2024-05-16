8ed00662f8caf981f7de0d765c821022e19701b7
Eternity0 <eternity211@126.com>
2024-05-16 21:46:23 +0800

    prettify the code and fix minor bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
@@ -8,120 +8,139 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 public class ManualOpMode extends LinearOpMode {
     RobotHardware hardware;
 
+    static double DUMP_JUDGING_THRESHOLD = 0.6;
+    static double LIFT_EXTRACT_POWER = 1;
+    static double LIFT_RETRACT_POWER = -1;
+    static double ARM_EXTRACT_POWER = -0.5;
+    static double ARM_RETRACT_POWER = 0.5;
+    static double INTAKE_ROLL_IN_POWER = -0.9;
+    static double INTAKE_ROLL_OUT_POWER = 0.9;
+    static double HOLDER_OPEN_POSITION = 0.5;
+    static double HOLDER_CLOSE_POSITION = 0;
+    static double DRONE_OPEN_POSITION = 0.64;
+    static double DRONE_CLOSE_POSITION = 0.94;
+    static double CYCLE_TIME_SECONDS = 0.025;
+    static double DUMP_STRETCHING_FAST_THRESHOLD = 0.43;
+    static double DUMP_RETRACTING_FAST_THRESHOLD = 0.95;
+    static double DUMP_STRETCHED_POSITION = 0.4;
+    static double DUMP_RETRACTED_POSITION = 0.98;
+    static double DUMP_CHANGING_OFFSET = 0.025;
+
+
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
         hardware = new RobotHardwareImpl(this);
 
         hardware.initMovement();
 
         waitForStart();
 
-        boolean dump;
-        boolean on_stretch = false;
-        boolean on_recovering = false;
-        double start_time = 0;
-        double dumpPosition = hardware.getDumpPosition();
+        DumpState dumpState;
+        double checkpoint = 0;
+        double targetDumpPosition = hardware.getDumpPosition();
         Gamepad gamepad2Snapshot = new Gamepad();
         gamepad2Snapshot.fromByteArray(gamepad2.toByteArray());
-        if (hardware.getDumpPosition() > 0.6) {
-            dump = false;
+        if (hardware.getDumpPosition() > DUMP_JUDGING_THRESHOLD) {
+            dumpState = DumpState.STRETCHED;
         } else {
-            dump = true;
+            dumpState = DumpState.RETRACTED;
         }
 
         while (opModeIsActive()) {
             double timer = getRuntime();
             if (gamepad2.dpad_up && gamepad2.right_bumper) {
-                hardware.setLiftPower(1);
+                hardware.setLiftPower(LIFT_EXTRACT_POWER);
             } else if (gamepad2.dpad_up && !gamepad2.right_bumper && hardware.getLiftPosition() < 2600) {
-                hardware.setLiftPower(-1);
+                hardware.setLiftPower(LIFT_RETRACT_POWER);
             } else {
                 hardware.setLiftPower(0);
             }
 
             if (gamepad2.b && gamepad2.right_bumper) {
-                hardware.setArmPower(-0.5);
+                hardware.setArmPower(ARM_EXTRACT_POWER);
             } else if (gamepad2.b && !gamepad2.right_bumper) {
-                hardware.setArmPower(0.5);
+                hardware.setArmPower(ARM_RETRACT_POWER);
             } else {
                 hardware.setArmPower(0);
             }
 
             if (gamepad2.x && gamepad2.right_bumper) {
-                hardware.setIntakePower(-0.9);
+                hardware.setIntakePower(INTAKE_ROLL_OUT_POWER);
             } else if (gamepad2.x && !gamepad2.right_bumper) {
-                hardware.setIntakePower(0.9);
+                hardware.setIntakePower(INTAKE_ROLL_IN_POWER);
             } else {
                 hardware.setIntakePower(0);
             }
 
             if (gamepad2.a) {
-                hardware.setHolderPosition(0.5);
+                hardware.setHolderPosition(HOLDER_OPEN_POSITION);
             } else {
-                hardware.setHolderPosition(0);
+                hardware.setHolderPosition(HOLDER_CLOSE_POSITION);
             }
 
-            if(!on_recovering && !on_stretch) {
-                if (hardware.getDumpPosition() > 0.6) {
-                    dump = false;
-                } else {
-                    dump = true;
+            if (gamepad2.y && !gamepad2Snapshot.y && dumpState.isAtIdle()) {
+                checkpoint = timer;
+                switch (dumpState) {
+                    case RETRACTED:
+                        dumpState = DumpState.STRETCHING;
+                        break;
+                    case STRETCHED:
+                        dumpState = DumpState.RETRACTING;
+                        break;
+                }
+                targetDumpPosition = hardware.getDumpPosition();
+            }
+
+
+            if (timer - checkpoint >= CYCLE_TIME_SECONDS) {
+                switch (dumpState) {
+                    case STRETCHING:
+                        if (hardware.getDumpPosition() >= DUMP_STRETCHING_FAST_THRESHOLD) {
+                            targetDumpPosition -= DUMP_CHANGING_OFFSET;
+                            checkpoint = timer;
+                        } else if (hardware.getDumpPosition() < DUMP_STRETCHING_FAST_THRESHOLD) {
+                            targetDumpPosition = DUMP_STRETCHED_POSITION;
+                            dumpState = DumpState.STRETCHED;
+                        }
+                        break;
+                    case RETRACTING:
+                        if (hardware.getDumpPosition() <= DUMP_RETRACTING_FAST_THRESHOLD) {
+                            targetDumpPosition += DUMP_CHANGING_OFFSET;
+                            checkpoint = timer;
+                        } else if (hardware.getDumpPosition() >= DUMP_RETRACTING_FAST_THRESHOLD) {
+                            targetDumpPosition = DUMP_RETRACTED_POSITION;
+                            dumpState = DumpState.RETRACTED;
+                        }
+                        break;
                 }
             }
 
-            if (gamepad2.y && !gamepad2Snapshot.y && !on_recovering && !on_stretch) {
-                dump = !dump;
-                if (dump) {
-                    start_time = getRuntime() - 0.25;
-                    on_stretch = true;
-                    dumpPosition = hardware.getDumpPosition();
-                }else{
-                    start_time = getRuntime() - 0.25;
-                    on_recovering = true;
-                    dumpPosition = hardware.getDumpPosition();
-                }
-            }
+            hardware.setDumpPosition(targetDumpPosition);
 
-            if((dump && hardware.getDumpPosition() > 0.6) || (on_stretch && dump)){
-                if(timer - start_time >= 0.025 && hardware.getDumpPosition() >= 0.43){
-                    dumpPosition -= 0.025;
-                    start_time = getRuntime();
-                } else if (timer-start_time >= 0.25 && dumpPosition <= 0.43) {
-                    dumpPosition = 0.4;
-                    on_stretch = false;
-                }
-                hardware.setDumpPosition(dumpPosition);
-            }
-            if ((!dump && hardware.getDumpPosition() < 0.6) || (on_recovering && !dump)) {
-                if (timer - start_time >= 0.05 && dumpPosition <= 0.95) {
-                    dumpPosition += 0.025;
-                    start_time = getRuntime();
-                }
-                else if (timer - start_time >= 0.025 && dumpPosition >= 0.95) {
-                    dumpPosition = 0.98;
-                    on_recovering = false;
-                }
-                hardware.setDumpPosition(dumpPosition);
-            }
-            telemetry.addData("dumpPosition",dumpPosition);
+            telemetry.addData("targetDumpPosition", targetDumpPosition);
 
             if (gamepad2.dpad_down) {
-                hardware.setDronePosition(0.75);
+                hardware.setDronePosition(DRONE_OPEN_POSITION);
             } else {
-                hardware.setDronePosition(1);
+                hardware.setDronePosition(DRONE_CLOSE_POSITION);
             }
 
-            hardware.driveRobot(
-                    gamepad1.left_stick_y,
-                    gamepad1.left_stick_x,
-                    gamepad1.right_stick_x);
+            hardware.driveRobot(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
-            telemetry.addData("distance",hardware.getDistance());
+            telemetry.addData("distance", hardware.getDistance());
             gamepad2Snapshot.fromByteArray(gamepad2.toByteArray());
             telemetry.update();
             sleep(10);
         }
 
         hardware.stopMotor();
+    }
+
+    enum DumpState {
+        RETRACTED, STRETCHED, RETRACTING, STRETCHING;
+
+        public boolean isAtIdle() {
+            return this == RETRACTED || this == STRETCHED;
+        }
     }
 }
